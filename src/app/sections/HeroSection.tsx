e53f537d120aa06e58930ebe12a744ed65378655
dumvan <felizavndrd567@gmail.com>
2023-09-19 17:52:50 +0800

    feat: integreate client cms

--- src/app/sections/HeroSection.tsx
+++ src/app/sections/HeroSection.tsx
@@ -13,18 +13,20 @@ import { Button } from 'antd'
 import { DownloadOutlined, GithubOutlined, LinkedinOutlined } from '@ant-design/icons'
 
 const ProggressbarIcon = () => {
-  const { theme } = useTheme()
-  return <span className='absolute h-16 w-12 bottom-0 left-1/2 transform -translate-x-1/2'>
-    {theme === "dark" ? (<Lottie animationData={scrollLottieDark} loop={true} />) : (<Lottie animationData={scrollLottieLight} loop={true} />)}
-  </span>
-}
+  const { theme } = useTheme();
+  return (
+    <span className={`mx-auto first-letter:h-16 w-12 bottom-0 left-1/2 transform -translate-x-1/2 ${theme === 'dark' ? 'md:absolute md:bottom-0 md:left-1/2 md:-translate-x-1/2' : ''}`}>
+      {theme === 'dark' ? <Lottie animationData={scrollLottieDark} loop={true} /> : <Lottie animationData={scrollLottieLight} loop={true} />}
+    </span>
+  );
+};
 
 
 const Hero = () => {
   return (
     <div className=" max-w-[62rem] px-10 sm:px-0 md:px-0 lg:px-0 py-5 mx-auto min-h-[100vh] h-[100vh]">
-      <div className="relative grid items-center justify-center grid-cols-12 gap-3 h-full">
-        <div className='col-span-12'>
+      <div className="relative flex flex-col items-center justify-center grid-cols-12 gap-3 h-full w-full">
+        <div className='mt-[7rem] w-full h-full'>
           <h3 className='relative text-6xl font-extrabold tracking-wide m-0'>Hello, My name is Evan.</h3>
           <p className='text-3xl font-bold tracking-widest '>
             I am a <span className='font-extrabold'>front-end developer</span> passionate for creating user experiences in the <span className='font-extrabold'>web</span>.
