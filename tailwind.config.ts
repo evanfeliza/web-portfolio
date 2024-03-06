0e122b1a6dd89bfea7588a3c747d12af5cd31cb3
dumvan <felizavndrd567@gmail.com>
2024-03-07 04:31:15 +0800

    chore: update packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,13 +2,29 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+     ],
   theme: {
-    extend: {},
+    extend: {
+       
+    },
   },
   plugins: [require("daisyui")],
+
+daisyui: {
+  themes: ["lofi","black"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+  darkTheme: "black", // name of one of the included themes for dark mode
+  base: true, // applies background color and foreground color for root element by default
+  styled: true, // include daisyUI colors and design decisions for all components
+  utils: true, // adds responsive and modifier utility classes
+  prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+  logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  themeRoot: ":root", // The element that receives theme color CSS variables
+},
+
 };
+
+
 export default config;
