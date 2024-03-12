12c8bf546a10ded6c5b8c17fced0a94338af7951
dumvan <felizavndrd567@gmail.com>
2024-03-12 20:24:17 +0800

    chore(app): remove navbar and footer from tree

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,22 @@
-
 import HeroSection from "@/pages/hero-section";
-import Navbar from "@/components/utils/navigations/navigation"
+
 import AboutSection from "@/pages/about-section";
-import HoverNavbar from "@/components/utils/navigations/hover-navigation";
+
 import SkillSection from "@/pages/skills-section";
 import ContactSection from "@/pages/contact-section";
-import FooterSection from "@/pages/footer-section";
 
-export default function Home() {
+
+export default async function Home() {
 
     return (
         <section className="min-h-screen min-w-screen grid grid-cols-1">
-            <Navbar />
-            <HoverNavbar />
             <div className="mx-auto">
                 <HeroSection />
                 <AboutSection />
                 <SkillSection />
                 <ContactSection />
-
-                <FooterSection />
             </div>
         </section>
+
     );
 }
