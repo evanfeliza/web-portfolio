0153d2cf1a6553d763e997f4fa2eca38c6e93ca4
dumvan <felizavndrd567@gmail.com>
2024-03-12 23:32:25 +0800

    chore(social-link): fix not redirecting

--- components/utils/links/social-link.tsx
+++ components/utils/links/social-link.tsx
@@ -9,19 +9,19 @@ const SocialLink = ({ type, url }: {
         {
             type === "github" ?
                 <li>
-                    <a href={`/${url}`} target="_blank">
+                    <a href={`${url}`} target="_blank">
                         <i className="fi fi-brands-github"></i>
                     </a>
                 </li> : type === "linkedin" ? <li>
-                    <a href={`/${url}`} target="_blank">
+                    <a href={`${url}`} target="_blank">
                         <i className="fi fi-brands-linkedin"></i>
                     </a>
                 </li> : type === "google" ? <li>
-                    <a href={`/mailto:${url}`}>
+                    <a href={`mailto:${url}`}>
                         <i className="fi fi-brands-google"></i>
                     </a>
                 </li> : type === "messenger" ? <li>
-                    <a href={`/${url}`} target="_blank">
+                    <a href={`${url}`} target="_blank">
                         <i className="fi fi-brands-facebook-messenger"></i>
                     </a>
                 </li> : null
