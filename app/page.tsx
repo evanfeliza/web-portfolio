37cca8920d420095e80cdbce063a1be919d5ee08
dumvan <felizavndrd567@gmail.com>
2024-03-15 00:10:09 +0800

    fix(app): page not responsive

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ import ContactSection from "@/pages/contact-section";
 
 export default async function Home() {
     return (
-        <section className="min-h-screen min-w-screen grid grid-cols-1">
-            <div className="mx-auto">
+        <section className="min-h-screen min-w-screen  grid grid-cols-1">
+            <div className="mx-auto h-full w-full">
                 <HeroSection />
                 <AboutSection />
                 <SkillSection />
