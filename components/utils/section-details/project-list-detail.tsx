715ac19cc8d5d42c2efaf6bb0ac1349ca060aa9f
dumvan <felizavndrd567@gmail.com>
2024-03-15 00:11:57 +0800

    chore(skill-section): update data

---
+++ components/utils/section-details/project-list-detail.tsx
@@ -0,0 +1,104 @@
+"use client"
+import { useEffect, useState } from 'react'
+import Image from 'next/image';
+import client from '@/components/src/sanity/sanity.client';
+import { groq } from 'next-sanity';
+import Card from '../cards/card';
+
+
+
+
+
+type ProjectData = {
+    projectCodeLink: string;
+    projectDemoLink: string
+    projectImage: string;
+    projectTitle: string;
+    techTags: string[]
+}
+
+type Projects = {
+    projects: ProjectData[]
+}
+
+const getProjectDetails = async () => {
+    const data = await client.fetch(groq`*[_type == "profile" && fullName == "Evan Feliza"]{projects[]{
+        projectCodeLink,
+        projectDemoLink,
+        projectTitle,
+        "projectImage": projectImage.asset->url,
+        techTags
+      }}`);
+    return data[0];
+};
+
+
+const useGetProjectDetailsData = () => {
+    const [projectData, setProjectData] = useState<ProjectData[]>([])
+    useEffect(() => {
+        const getProjectsData = async () => {
+            const res = await getProjectDetails();
+            setProjectData([...res?.projects]);
+        };
+        getProjectsData()
+
+    }, [])
+    return projectData
+}
+
+
+const ProjectCard = ({ data }: { data: ProjectData }) => {
+    const [cardData] = useState<ProjectData>(data)
+
+    return (
+        <div data-aos="fade-up" className="h-full relative group ">
+            <figure>
+                <Image
+                    src={`${cardData?.projectImage}`}
+                    alt={`${cardData?.projectTitle}`}
+                    width={1280}
+                    height={720}
+                    quality={100}
+                    className="object-cover w-full h-full group-hover:opacity-75 transition-opacity duration-300"
+                />
+            </figure>
+
+            <div className=" absolute inset-0 flex gap-2 items-center flex-col lg:flex-row justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <div className='join'>
+                    <button className='btn join-item'>
+                        <a href={`${cardData?.projectCodeLink}`} target="_blank" >
+                            <i className="fi fi-brands-github"></i>
+                        </a>
+                    </button>
+
+                    <button className='btn join-item'>
+                        <a href={`${cardData?.projectDemoLink}`} target="_blank" >
+                            <i className="fi fi-rr-share"></i>
+                        </a>
+                    </button>
+                </div>
+                <div className='absolute bg-base-300 bottom-0 left-0 px-2 py-4 w-full drop-shadow-sm'>
+                    <h1 className='uppercase tracking-wider font-semibold'>{cardData.projectTitle}</h1>
+                    <div className='grid grid-cols-5 gap-2 mt-4'>
+                        {cardData?.techTags.map(techTag => <span key={techTag} className='badge badge-ghost text-xs' >{`#${techTag}`}</span>)}
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+
+const ProjectList = () => {
+    const projectData = useGetProjectDetailsData()
+    return (
+        <div data-aos="fade-up" className='grid grid-cols-1 lg:grid-cols-2 max-h-[25.5rem]  overflow-y-auto no-scrollbar'>
+            {projectData?.map(data => <ProjectCard data={data} key={data?.projectTitle} />)}
+        </div>
+    )
+}
+
+export default ProjectList
--- pages/skills-section.tsx
+++ pages/skills-section.tsx
@@ -1,4 +1,5 @@
 import Card from '@/components/utils/cards/card'
+import ProjectList from '@/components/utils/section-details/project-list-detail'
 import TechStackDetail from '@/components/utils/section-details/tech-stack-detail'
 import React from 'react'
 
@@ -9,45 +10,18 @@ const SkillSection = () => {
     return (
         <section id="skills" className='max-h-full max-w-full bg-base-200 w-full border-t'>
             <div className='px-6 lg:px-12 py-6'>
-                <div className='mx-auto'>
+                <div data-aos="fade-up" className='mx-auto'>
                     <h3 className="text-8xl tracking-tighter font-medium">skills.</h3>
                     <p className='text-md'> Here are some of my projects that I have made and list of the list of technologies used.</p>
                 </div>
 
                 <div className='grid h-full mt-8'>
                     <div className="relative grid grid-cols-[15%_1fr] lg:grid-cols-[5%_1fr] ">
-                        <span className=' [writing-mode:vertical-lr] flex items-end justify-end transform rotate-180 text-5xl uppercase font-medium tracking-widest '>my projects.</span>
-                        <div className='grid grid-cols-1 lg:grid-cols-2 max-h-[25.5rem]  overflow-y-auto no-scrollbar'>
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-                            <Card />
-
-                        </div>
+                        <div className=' [writing-mode:vertical-lr] flex items-end justify-end transform rotate-180 text-5xl uppercase font-medium tracking-widest '><span data-aos="fade-down" data-aos-delay="1000" >my projects</span></div>
+                        <ProjectList />
                     </div>
-
                 </div>
-
                 <TechStackDetail />
-
-
-
             </div>
 
         </section>
