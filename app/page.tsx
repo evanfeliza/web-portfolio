8d5f97e565629e547538e9ff420a27019eb8b472
dumvan <felizavndrd567@gmail.com>
2024-03-28 22:09:12 +0800

    chore: add navbar and footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,24 @@ import AboutSection from "@/pages/about-section";
 import SkillSection from "@/pages/skills-section";
 import ContactSection from "@/pages/contact-section";
 import Loader from "@/components/utils/loaders/loader";
+import FooterSection from "@/pages/footer-section";
+import Navbar from "@/components/utils/navigations/navigation";
+import HoverNavbar from "@/components/utils/navigations/hover-navigation";
 
 
 
 export default async function Home() {
     return (
-        <section className="relative min-h-screen min-w-screen  grid grid-cols-1">
+        <section className="relative h-screen min-w-screen">
             <Loader />
-            <div className="mx-auto">
+            <div className="container mx-auto">
+                <Navbar />
+                <HoverNavbar />
                 <HeroSection />
                 <AboutSection />
                 <SkillSection />
                 <ContactSection />
+                <FooterSection />
             </div>
         </section>
 
