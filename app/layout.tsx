f7d7def008bbe3b0859de5dcab08e5bac0c8f792
dumvan <felizavndrd567@gmail.com>
2024-03-28 22:08:41 +0800

    chore(app-layout): navbar and footer moved to app page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import 'tailwindcss/tailwind.css';
 import type { Metadata } from "next";
 import "./globals.css"
 import AnimateOnScroll from '@/components/utils/animations/aos';
-import Navbar from "@/components/utils/navigations/navigation"
-import HoverNavbar from "@/components/utils/navigations/hover-navigation";
-import FooterSection from "@/pages/footer-section";
 
 export const metadata: Metadata = {
   title: "Evan Feliza",
@@ -24,10 +21,7 @@ export default function RootLayout({
       <AnimateOnScroll />
       <body>
         <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
-        <Navbar />
-        <HoverNavbar />
         {children}
-        <FooterSection />
       </body>
     </html>
   );
