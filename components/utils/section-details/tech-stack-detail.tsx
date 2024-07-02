4cd01c6a809672263638b13e70fe796ec8eb7b42
dumvan <felizavndrd567@gmail.com>
2024-07-03 07:32:25 +0800

    chore(tech-stack-detail): add tooltip

--- components/utils/section-details/tech-stack-detail.tsx
+++ components/utils/section-details/tech-stack-detail.tsx
@@ -31,7 +31,7 @@ const useGetProjectDetailsData = () => {
 
 
 const TechStackCard = ({ name }: { name: string }) => {
-    return (<div className='h-100 w-100 grayscale-[80%] cursor-pointer hover:grayscale-0 duration-300 flex items-center justify-center mt-4'>
+    return (<div data-tip={`${name}`} className='h-100 w-100 grayscale-[80%] tooltip tooltip-top capitalize   cursor-pointer hover:grayscale-0 duration-300 flex items-center justify-center mt-4'>
         <i className={`text-4xl text-base-300 devicon-${name}-plain colored mr-3`}></i>
         <span className='hidden lg:block text-sm font-bold  uppercase tracking-wider'>{name}</span>
     </div>)
@@ -43,7 +43,7 @@ const TechStackDetail = () => {
     return (
 
         <div data-aos="fade-in" data-aos-duration="2000" className="mt-5 mx-auto max-w-[30rem] lg:max-w-[90rem] grid grid-cols-2 lg:grid-cols-3 items-center gap-10">
-            {data?.techStack?.map(stack => <TechStackCard key={stack} name={stack} />)}
+            {data?.techStack?.map(stack => (<TechStackCard key={stack} name={stack} />))}
         </div>
     )
 }
