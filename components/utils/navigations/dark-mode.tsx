5e244f30dc7f48e65ee132734e5ea953aa5feb5b
dumvan <felizavndrd567@gmail.com>
2024-07-09 09:14:35 +0800

    chore(dark-mode): fix flicker on render

--- components/utils/navigations/dark-mode.tsx
+++ components/utils/navigations/dark-mode.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 
 
 const DarkMode = () => {
-    const [theme, setTheme] = useState('lofi')
+    const [theme, setTheme] = useState('black')
     useEffect(() => {
         document.querySelector("html")?.setAttribute("data-theme", theme)
         document.querySelector("html")?.classList.add("duration-100")
@@ -19,8 +19,6 @@ const DarkMode = () => {
             <input onClick={toggleTheme} type='checkbox' />
             <i className="swap-on fi fi-ss-moon-stars"></i>
             <i className="swap-off fi fi-ss-brightness"></i>
-
-            <i></i>
         </label>
     )
 }
