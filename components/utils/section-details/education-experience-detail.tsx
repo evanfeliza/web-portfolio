1efa04b9b50ee6670551c19bf3e7a55b0ad7ee96
dumvan <felizavndrd567@gmail.com>
2024-08-02 10:35:47 +0800

    chore(education-experience-detail): update mobile deign layout

--- components/utils/section-details/education-experience-detail.tsx
+++ components/utils/section-details/education-experience-detail.tsx
@@ -46,19 +46,29 @@ const DesktopTimeline = ({ timelineData }: { timelineData?: Timeline[] }) => {
 }
 
 const MobileTimeline = ({ timelineData }: { timelineData?: Timeline[] }) => {
-
-
     return (
-        <ul className='grid grid-cols-1 gap-4'>
-            {timelineData?.map((data, index) => (<li key={index} className='card card-compact bg-base-100'>
-                <div className='card-body !gap-1'>
-                    <div className='text-2xl'><GeneratedIcon icon={data?.logo?.code as string} /></div>
-                    <p className='text-md font-normal break-normal tracking-tight'>{data?.event}</p>
-                    <span className="text-md tracking-wider font-semibold mt-2 ">{data?.year}</span>
-                    <span className="mt-2 text-xs  tracking-wider"><i className="fi fi-ss-map-marker mr-2 "></i>{data?.location}</span>
-                </div>
-            </li>))}
-        </ul>
+        <div className="relative mt-10">
+            <div className="border-r-4 border-accent absolute h-full top-0" style={{ 'left': '9px' }}></div>
+            <ul className="list-none m-0 p-0 ">
+                {timelineData?.map((timeline, index) => <li key={index} className="mb-5">
+                    <div className="flex group items-center ">
+                        <div className="bg-base-content group-hover:bg-base-200 group-hover:duration-300 z-10 rounded-full border-4 border-accent h-5 w-5">
+                            <div className="bg-accent h-1 w-6 items-center  ml-4 mt-1"></div>
+                        </div>
+                        <div className="flex-1 ml-4 z-10 font-medium ">
+                            <div className="order-1 space-y-2 bg-accent rounded-lg shadow-only transition-ease w-12/12 px-6 py-4">
+                                <div className='text-2xl text-accent-content'><GeneratedIcon icon={timeline?.logo?.code as string} /></div>
+                                <h3 className="mb-3 font-bold text-accent-content text-2xl">{timeline?.event}</h3>
+                                <p className="pb-4 text-sm text-accent-content">{timeline?.year}</p>
+                                <hr />
+                                <p className="text-sm font-medium leading-snug tracking-wide text-gray-300 text-opacity-100">{timeline?.location}</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                )}
+            </ul>
+        </div>
     )
 }
 
@@ -70,9 +80,10 @@ const EducationAndExperiencesDetail = async () => {
 
                 <div className='hidden lg:flex items-center w-full'>
                     <DesktopTimeline timelineData={timelineData} />
-                    <div className='pb-4 mx-auto' ><span className='tracking-widest uppercase font-semibold text-5xl'>my timeline</span></div>
+                    <div className='mx-auto' ><span className='tracking-widest uppercase font-semibold text-5xl'>my timeline</span></div>
                 </div>
                 <div className="block lg:hidden">
+                    <div className='text-center' ><span className='tracking-widest uppercase font-semibold text-3xl mx-auto'>my timeline</span></div>
                     <MobileTimeline timelineData={timelineData} />
                 </div>
             </div>
