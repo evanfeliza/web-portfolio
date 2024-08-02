78eb0c7ec8b24755392a2eb7d51adb6cfca2e19b
dumvan <felizavndrd567@gmail.com>
2024-08-02 09:35:42 +0800

    chore(splash-screen) remove excessive animations

--- components/utils/animations/splash-screen.tsx
+++ components/utils/animations/splash-screen.tsx
@@ -6,15 +6,15 @@ const SplashScreen = () => {
     useEffect(() => {
         if (typeof window !== 'undefined') {
             const loader = document.getElementById('splash-screen');
-            if (loader)
-                loader.classList.add('-translate-y-[100vh]', 'opacity-1')
-            setTimeout(() => {
+            if (loader) {
                 loader?.remove();
-            }, 5000)
+            }
+
+
         }
     }, []);
     return (
-        <div id="splash-screen" className={`fixed bg-base-100 h-full w-full flex items-center justify-center z-50 transition-transform duration-[2500ms] lg:duration-[3000ms] `}>
+        <div id="splash-screen" className={`fixed bg-base-100 h-full w-full flex items-center justify-center z-50 transition-transform`}>
             <span className='loader'></span>
         </div>
     )
