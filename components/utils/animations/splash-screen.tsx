d50b9c057926f7e39c0b725d016107f3e94c0ec6
dumvan <felizavndrd567@gmail.com>
2024-08-07 03:04:20 +0800

    chore(splash-screen): fix page not responsive after transition

--- components/utils/animations/splash-screen.tsx
+++ components/utils/animations/splash-screen.tsx
@@ -8,13 +8,16 @@ const SplashScreen = () => {
             if (loader) {
                 setTimeout(() => {
                     loader.classList.add('transition-opacity', 'ease-in', 'opacity-0');
+                    setTimeout(() => {
+                        loader.remove()
+                    }, 2000)
                 }, 3000);
             }
         }
     }, []);
 
     return (
-        <div id="splash-screen" className="fixed bg-base-100 duration-700 h-full w-full flex items-center justify-center z-50">
+        <div id="splash-screen" className="fixed bg-base-100 duration-500 h-full w-full flex items-center justify-center z-50">
             <span className='loader'></span>
         </div>
     );
